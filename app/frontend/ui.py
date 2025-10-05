8d0690b1801a2ea058c915e55f4e756eec491fea
luch91 <61505908+luch91@users.noreply.github.com>
2025-10-05 06:52:47 +0100

    resolved port issue

--- app/frontend/ui.py
+++ app/frontend/ui.py
@@ -17,7 +17,7 @@ allow_web_search = st.checkbox("Allow web search")
 
 user_query = st.text_area("Enter your query : " , height=150)
 
-API_URL = "http://127.0.0.1:9000/chat"
+API_URL = "http://127.0.0.1:8000/chat"
 
 if st.button("Ask Agent") and user_query.strip():
 
